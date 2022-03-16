c7b88bf377141b06f8e3c520e83bc30c2bc545f0
Eric Davis <eric36davis@icloud.com>
2022-03-16 14:31:21 +0100

    Initial Commit

---
+++ Weather App/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Eric Davis on 16/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
