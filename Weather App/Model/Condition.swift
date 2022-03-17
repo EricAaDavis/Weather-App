1ff737f6c5b68d97a2859e66971abb22a4f713aa
Eric Davis <eric36davis@icloud.com>
2022-03-17 10:34:57 +0100

    add condition

---
+++ Weather App/Model/Condition.swift
@@ -0,0 +1,20 @@
+//
+//  Condition.swift
+//  Weather App
+//
+//  Created by Eric Davis on 17/03/2022.
+//
+
+import Foundation
+
+struct Main: Codable {
+    var temp: Float
+    var feels_like: Float
+    var temp_min: Float
+    var temp_max: Float
+    var pressure: Int
+    var humidity: Int
+    var sea_level: Int
+    var grnd_level: Int
+}
+   
